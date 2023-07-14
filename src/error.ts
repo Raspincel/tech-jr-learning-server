45d8158f1e5052f2b547b3f853b109498ca71cd4
Raspincel <raspincel@gmail.com>
2023-07-14 13:20:21 -0300

    feat: created custom error handling + log on database

---
+++ src/error.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from "express"
+import prisma from "./database"
+
+interface LogContent {
+    message: string
+    id?: string
+    email?: string
+}
+
+export class AppError extends Error {
+    public readonly status: number
+    public readonly log: LogContent
+
+    constructor(_status: number = 400, _message: string, _log: LogContent) {
+        super(_message)
+        this.status = _status
+        this.log = _log
+    }
+}
+
+export async function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+
+    if (err instanceof AppError) {
+        res.status(err.status).json({ message: err.message })
+
+        const { message, email, id } = err.log
+        const user = (id || email) ? {
+            connect: {
+                id: id ? id : "",
+                email: email ? email : ""
+            }
+        } : undefined
+
+        await prisma.log.create({
+            data: {
+                description: message,
+                user
+            }
+        })
+
+        return;
+    }
+
+    res.status(500).json({ message: "Error interno do servidor." })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
+import 'express-async-errors'
 import express from 'express'
 import cors from 'cors'
 import userRouter from './routes/user.routes'
+import { errorHandler } from './error'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use('/user', userRouter)
+
+app.use(errorHandler)
 
 export default app
